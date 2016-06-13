db1ae4897bd47e61b5f39227ee31faeab065e286
China Wang <chwang@thoughtworks.com>
2016-06-13 15:13:17 -0700

    fixed campaign display name in legend

--- js/action_kit.js
+++ js/action_kit.js
@@ -1,39 +1,40 @@
 function fetchActionKitData(campaignsString, callback) {
   var separator = '|';
-  var campaignNames = campaignsString.split(separator);
+  var campaigns = campaignsString.split(separator);
   var template =
-      '<div class="popup_box"> ' +
-        '<div class="popup_box_header"> ' +
-          '<strong><a href="{{ website }}">{{ name }}</a></strong> ' +
-          '</div> <em>{{ city }} {{ state }} {{country}}</em> <hr /> ' +
-          '{{ start_time }} <br> {{event_date}} <br><br> ' +
-          '{{ venue }} <br> {{ address }} <br> {{ city }} {{ state }} {{country}} <br><br>' +
-          '<strong><a href="{{ rsvp_url }}">RSVP</a></strong> </div>';
+    '<div class="popup_box"> ' +
+    '<div class="popup_box_header"> ' +
+    '<strong><a href="{{ website }}">{{ name }}</a></strong> ' +
+    '</div> <em>{{ city }} {{ state }} {{country}}</em> <hr /> ' +
+    '{{ start_time }} <br> {{event_date}} <br><br> ' +
+    '{{ venue }} <br> {{ address }} <br> {{ city }} {{ state }} {{country}} <br><br>' +
+    '<strong><a href="{{ rsvp_url }}">RSVP</a></strong> </div>';
   var compiledTemplate = Handlebars.compile(template);
   var layerGroups = {};
-  var ajaxRequests = [];
+  var actionKitRequests = [];
 
-  campaignNames.forEach(function(campaign) {
-    ajaxRequests.push($.ajax({
+  campaigns.forEach(function(campaign) {
+    actionKitRequests.push($.ajax({
       url: 'https://act.350.org/event/' + campaign + '?template_set=json_nearby_events&jsonp=?',
       dataType: 'jsonp',
       success: function (data) {
         var markers = L.layerGroup();
+        var campaignName = data.campaign || campaign;
         if (data.events) {
-          data.events.forEach(function (event) {
+          data.events.forEach(function(event) {
             if (event.latitude && event.longitude) {
               var marker = L.marker([event.latitude, event.longitude]);
               marker.bindPopup(compiledTemplate(event));
               markers.addLayer(marker);
             }
           });
-          layerGroups[campaign] = markers;
+          layerGroups[campaignName] = markers;
         }
       }
     }));
   });
 
-  $.when.apply($, ajaxRequests).done(function() {
+  $.when.apply($, actionKitRequests).done(function() {
     callback(layerGroups);
   });
 }
