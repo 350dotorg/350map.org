2d135de6975f688199aab9e2d496993c8e5c03c8
TC <tainacaetano@gmail.com>
2016-07-21 12:48:07 -0700

    more cleanup

--- js/control-shift-service.js
+++ js/control-shift-service.js
@@ -1,29 +1,34 @@
+var urlSeparator = ':';
+var categorySeparator = '|';
+var compiledTemplate = Handlebars.compile(getControlShiftTemplate());
+
+var firstPageRequests = [];
+var allPageRequests = [];
+
+var layerGroups = {};
+
 function fetchControlShiftData(categoriesString, callback) {
   if (!categoriesString) {
     callback({});
     return;
   }
 
-  var urlSeparator = ':';
-  var categorySeparator = '|';
   var categories = categoriesString.split(categorySeparator);
+  
+  var categoryPageNumberMap = getPagesForEachCategory(categories);
+  
+  $.when.apply($, firstPageRequests).done(function() {
+    categories.forEach(function(categoryInfo) {
+      createCampaignMarkersForCategory(categoryInfo, categoryPageNumberMap);
+    });
 
-  var template =
-    '<div class="popup_box">' +
-    '<div class="popup_box_header">' +
-    '<strong><a target="_blank" href="{{ url }}">{{ title }}</a></strong>' +
-    '</div><div><em>{{ location.query }}</em></div><hr />' +
-    '{{ who }}<br><br>' +
-    '{{ what }} <br><br>' +
-    '{{ signature_count }} out of {{ goal }} signatures' +
-    '</div>';
-  var compiledTemplate = Handlebars.compile(template);
+    renderMarkers(callback);
+  });
+}
 
-  var firstPageRequests = [];
-  var allPageRequests = [];
+function getPagesForEachCategory(categories) {
   var categoryPageNumberMap = {};
-  var layerGroups = {};
-
+  
   categories.forEach(function(categoryInfo) {
     var domain = categoryInfo.split(urlSeparator)[0];
     var category = categoryInfo.split(urlSeparator)[1];
@@ -37,45 +42,50 @@ function fetchControlShiftData(categoriesString, callback) {
     }));
   });
 
-  $.when.apply($, firstPageRequests).done(function() {
-    categories.forEach(function(categoryInfo) {
-      var domain = categoryInfo.split(urlSeparator)[0];
-      var category = categoryInfo.split(urlSeparator)[1];
-      var totalPages = categoryPageNumberMap[categoryInfo];
+  return categoryPageNumberMap;
+}
 
-      for (var i = 1; i <= totalPages; i++) {
-        var requestUrl = getControlShiftUrl(domain, category, i);
+function createCampaignMarkersForCategory(categoryInfo, categoryPageNumberMap) {
+  var domain = categoryInfo.split(urlSeparator)[0];
+  var category = categoryInfo.split(urlSeparator)[1];
+  var totalPages = categoryPageNumberMap[categoryInfo];
 
-        allPageRequests.push($.ajax({
-          url: requestUrl,
-          dataType: 'jsonp',
-          success: function(data) {
-            var markers = L.layerGroup();
-            var categoryName = data.name || category;
-            if (data.results) {
-              data.results.forEach(function(petition) {
-                if (petition.location) {
-                  var location = petition.location;
-                  if (location.latitude && location.longitude) {
-                    var marker = L.marker([location.latitude, location.longitude], {icon: getPetitionIcon()});
-                    marker.bindPopup(compiledTemplate(petition));
-                    markers.addLayer(marker);
-                  }
-                }
-              });
-              layerGroups[categoryName] = markers;
-            }
-          }
-        }))
+  for (var i = 1; i <= totalPages; i++) {
+    var requestUrl = getControlShiftUrl(domain, category, i);
+
+    allPageRequests.push($.ajax({
+      url: requestUrl,
+      dataType: 'jsonp',
+      success: function(data) {
+        createCampaignMarkers(data);
+      } 
+    }))
+  }
+}
+
+function createCampaignMarkers(data) {
+  var markers = L.layerGroup();
+  var categoryName = data.name || category;
+  if (data.results) {
+    data.results.forEach(function(petition) {
+      if (petition.location) {
+        var location = petition.location;
+        if (location.latitude && location.longitude) {
+          var marker = L.marker([location.latitude, location.longitude], {icon: getPetitionIcon()});
+          marker.bindPopup(compiledTemplate(petition));
+          markers.addLayer(marker);
+        }
       }
     });
+    layerGroups[categoryName] = markers;
+  }
+}
 
-    $.when.apply($, allPageRequests).done(function() {
+function renderMarkers(callback) {
+  $.when.apply($, allPageRequests).done(function() {
       callback(layerGroups);
     });
-  });
-
-  function getControlShiftUrl(domain, category, page) {
-    return 'https://' + domain + '/categories/' + category + '.json?page=' + page;
-  }
+}
+function getControlShiftUrl(domain, category, page) {
+  return 'https://' + domain + '/categories/' + category + '.json?page=' + page;
 }
---
+++ js/template-service.js
@@ -0,0 +1,21 @@
+function getActionKitTemplate() {
+	return '<div class="popup_box"> ' +
+	    '<div class="popup_box_header"> ' +
+	    '<strong><a target="_blank" href="{{ rsvp_url }}">{{ name }}</a></strong> ' +
+	    '</div> <em>{{ city }}{{#if state }}, {{/if }}{{ state }}{{#if country }},{{/if }} {{ country }}</em> <hr /> ' +
+	    '{{ start_time }} <br> {{ event_date }} <br><br> ' +
+	    '{{ venue }} <br> {{ address }} <br> {{ city }}{{#if state }}, {{/if }}{{ state }}{{#if country }},{{/if }} {{ country }} <br><br>' +
+	    '<strong><a href="{{ rsvp_url }}">RSVP</a></strong> </div>';
+
+}
+
+function getControlShiftTemplate() {
+	return '<div class="popup_box">' +
+	    '<div class="popup_box_header">' +
+	    '<strong><a target="_blank" href="{{ url }}">{{ title }}</a></strong>' +
+	    '</div><div><em>{{ location.query }}</em></div><hr />' +
+	    '{{ who }}<br><br>' +
+	    '{{ what }} <br><br>' +
+	    '{{ signature_count }} out of {{ goal }} signatures' +
+	    '</div>';
+}
