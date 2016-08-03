6ae472f42a58dd36fd8239b308d3c405c538c4ec
China Wang <chwang@thoughtworks.com>
2016-08-03 14:02:47 -0700

    new popup styles for actionkit and controlshift

--- js/template-service.js
+++ js/template-service.js
@@ -1,30 +1,36 @@
 function getActionKitTemplate() {
   return '<div class="popup_box">' +
+    '<div class="bg-blue">' +
     '<div class="popup_box_header">' +
+    '<div class="popup_box_type">Event</div>' +
     '<a target="_blank" href="{{ rsvp_url }}">{{ name }}</a>' +
     '</div>' +
-    '<div>{{ city }}{{#if state }}, {{/if }}{{ state }}{{#if country }},{{/if }} {{ country }}</div>' +
-    '<div>{{ start_time }}</div>' +
+    '</div>' +
+    '<div class="popup_box_info">' +
     '<div>{{ event_date }}</div>' +
-    '<div>{{ venue }}</div>' +
+    '<div>{{ start_time }}</div>' +
+    '<div class="space">{{ venue }}</div>' +
     '<div>{{ address }} </div>' +
-    '<div>{{ city }}{{#if state }}, {{/if }}{{ state }}{{#if country }},{{/if }} {{ country }}</div>' +
-    '<a href="{{ rsvp_url }}">RSVP</a>' +
+    '<div class="space-bottom">{{ city }}{{#if state }}, {{/if }}{{ state }}{{#if country }},{{/if }} {{ country }}</div>' +
+    '<div class="button arrow-right popup_button"><a class="popup_button_text" href="{{ rsvp_url }}">RSVP</a></div>' +
+    '</div>' +
     '</div>';
 
 }
 
 function getControlShiftTemplate() {
   return '<div class="popup_box">' +
-      '<div class="bg-blue" class="popup-header-text">' +
-        '<div class="popup_box_header">' +
-          '<div>PETITION</div>' +
-          '<strong><a target="_blank" href="{{ url }}">{{ title }}</a></strong>' +
-        '</div>' +
-      '</div>' +
-      '<div class="bold_margin">TO: {{ who }}</div>' +
-      '<div>{{ what }}</div>' +
-      '<div class="bold_margin">{{ signature_count }} signatures out of {{ goal }} needed.</div>' +
-      '<div class="button arrow-right"><a class="popup-button-text" target="_blank" href="{{ url }}">Sign the Petition</a></div>' +
+    '<div class="bg-blue">' +
+    '<div class="popup_box_header">' +
+    '<div class="popup_box_type">Petition</div>' +
+    '<a target="_blank" href="{{ url }}">{{ title }}</a>' +
+    '</div>' +
+    '</div>' +
+    '<div class="popup_box_info">' +
+    '<div class="bold">TO: {{ who }}</div>' +
+    '<div class="space">{{ what }}</div>' +
+    '<div class="bold space-bottom">{{ signature_count }} signatures out of {{ goal }} needed.</div>' +
+    '<div class="button arrow-right popup_button"><a class="popup_button_text" target="_blank" href="{{ url }}">Sign the Petition</a></div>' +
+    '</div>' +
     '</div>';
 }
